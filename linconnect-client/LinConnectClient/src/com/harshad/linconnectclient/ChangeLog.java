7e6e443acd0ec0fe0d70ea0c408ca4607132e8e5
hj91 <firewalrus@gmail.com>
2014-05-04 19:16:29 +0530

    adding new files after deleting unwanted stuff

---
+++ linconnect-client/LinConnectClient/src/com/harshad/linconnectclient/ChangeLog.java
@@ -0,0 +1,21 @@
+package com.harshad.linconnectclient;
+
+import android.app.Dialog;
+
+public class ChangeLog {
+
+	public ChangeLog(SettingsActivity settingsActivity) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isFirstRun() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Dialog getLogDialog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

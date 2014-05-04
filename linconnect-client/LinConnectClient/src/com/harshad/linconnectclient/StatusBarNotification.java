70ad41159441d5aed8bb5ce7ac4647015394361e
hj91 <firewalrus@gmail.com>
2014-05-04 13:38:21 +0530

    Added linconnect client code

---
+++ linconnect-client/LinConnectClient/src/com/harshad/linconnectclient/StatusBarNotification.java
@@ -0,0 +1,17 @@
+package com.harshad.linconnectclient;
+
+import android.app.Notification;
+
+public class StatusBarNotification {
+
+	public Notification getNotification() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPackageName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
